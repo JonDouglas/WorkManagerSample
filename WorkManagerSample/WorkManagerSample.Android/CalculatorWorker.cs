97b1d214ed5fa80be58e714d0ef75e52a9afe781
Jon Douglas <jodou@microsoft.com>
2019-04-05 10:13:50 -0600

    Initial commit

---
+++ WorkManagerSample/WorkManagerSample.Android/CalculatorWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Work;
+
+namespace WorkManagerSample.Droid
+{
+    public class CalculatorWorker : Worker
+    {
+        public CalculatorWorker(Context context, WorkerParameters workerParameters) : base(context, workerParameters)
+        {
+
+        }
+        public override Result DoWork()
+        {
+            var taxReturn = CalculateTaxes();
+            Android.Util.Log.Debug("CalculatorWorker", $"Your Tax Return is: {taxReturn}");
+            return Result.InvokeSuccess();
+        }
+
+        public double CalculateTaxes()
+        {
+            return 2000;
+        }
+    }
+}
---
+++ WorkManagerSample/WorkManagerSample.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using AndroidX.Work;
+
+namespace WorkManagerSample.Droid
+{
+    [Activity(Label = "WorkManagerSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            PeriodicWorkRequest taxWorkRequest = PeriodicWorkRequest.Builder.From<CalculatorWorker>(TimeSpan.FromMinutes(20)).Build();
+
+            WorkManager.Instance.Enqueue(taxWorkRequest);
+        }
+    }
+}
